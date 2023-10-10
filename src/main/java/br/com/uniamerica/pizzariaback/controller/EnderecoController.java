704bc26cd25ddfcce93e64c2884c6178b2e21279
João Victor Guillen <guillenguilli@gmail.com>
2023-10-10 20:29:02 -0300

    teste

--- src/main/java/br/com/uniamerica/pizzariaback/controller/EnderecoController.java
+++ src/main/java/br/com/uniamerica/pizzariaback/controller/EnderecoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// add comentario teste
 @RestController
 @RequestMapping (value = "/api/endereco")
 public class EnderecoController {
